79370a77313a59b98ab7da551e034f5065e0a0fa
ankkarp <anna.a.k.2002@gmail.com>
2022-07-03 10:17:40 +0300

    ESRGAN implementation

--- bot.py
+++ bot.py
@@ -1,12 +1,17 @@
 import logging
 from aiogram import Bot, Dispatcher, executor, types
+import glob
+import os
+import cv2
+import numpy as np
+import torch
+import RRDBNet_arch as arch
 # import torchvision.transforms as tt
 # from PIL import Image
 # from ninasr import *
 # from edrs import *
 # from rcan import *
 # import torch as th
-import os
 
 API_TOKEN = '5503345880:AAF_QgSgOHwrMlEY5ABAWajpsDhXxxe3DxM'
 
@@ -28,11 +33,23 @@ async def send_welcome(message: types.Message):
 
 
 @dp.message_handler(content_types=['photo'])
-async def vk_pfp_check(message):
+async def upscale(message):
     try:
         downloadable = message.photo[-1]
-        os.chdir(os.path.abspath('ESRGAN'))
-        await downloadable.download(f'LR/{message.chat.id}.png')
+        model = arch.RRDBNet(3, 3, 64, 23, gc=32)
+        model.load_state_dict(torch.load('RRDB_ESRGAN_x4.pth'), strict=True)
+        model.eval()
+        await downloadable.download(destination_file=f'{message.chat.id}.png')
+        img = cv2.imread(f'{message.chat.id}.png', cv2.IMREAD_COLOR)
+        img = img * 1.0 / 255
+        img = torch.from_numpy(np.transpose(img[:, :, [2, 1, 0]], (2, 0, 1))).float()
+        img_LR = img.unsqueeze(0)
+        with torch.no_grad():
+            output = model(img_LR).data.squeeze().float().cpu().clamp_(0, 1).numpy()
+        output = np.transpose(output[[2, 1, 0], :, :], (1, 2, 0))
+        output = (output * 255.0).round()
+        cv2.imwrite(f'{message.chat.id}.png', output)
+
         # img = Image.open(img_name)
         # lr_t = tt.ToTensor()(img)[:3]
         # model = edsr_baseline(2, pretrained=True)
@@ -41,10 +58,8 @@ async def vk_pfp_check(message):
         # sr_t = model(lr_t)
         # sr = tt.ToPILImage()(sr_t.squeeze(0))
         # sr.save(img_name)
-        os.system("python test.py")
-        await bot.send_photo(message.chat.id, types.InputFile(f'results/{message.chat.id}_rlt.png'))
-        os.remove(f'results/{message.chat.id}_rlt.png')
-        os.remove(f'LR/{message.chat.id}.png')
+        await bot.send_photo(message.chat.id, types.InputFile(f'{message.chat.id}.png'))
+        os.remove(f'{message.chat.id}.png')
         # bot.send_message(message.chat.id, 'Photo does not need rescaling')
     except Exception as e:
         print(e)
