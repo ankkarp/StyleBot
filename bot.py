2634df1e57538785d2f932fcc76b6658d17c0ae5
ankkarp <anna.a.k.2002@gmail.com>
2022-07-05 22:43:30 +0300

    fixed compression issue

--- bot.py
+++ bot.py
@@ -1,11 +1,12 @@
 import logging
 from aiogram import Bot, Dispatcher, executor, types
-import glob
 import os
 import cv2
 import numpy as np
 import torch
 import RRDBNet_arch as arch
+from stylizer import stylize
+import asyncio
 # import torchvision.transforms as tt
 # from PIL import Image
 # from ninasr import *
@@ -18,6 +19,7 @@ API_TOKEN = '5503345880:AAF_QgSgOHwrMlEY5ABAWajpsDhXxxe3DxM'
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 
+loop = asyncio.get_event_loop()
 # Initialize bot and dispatcher
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
@@ -28,63 +30,51 @@ async def send_welcome(message: types.Message):
     """
     This handler will be called when user sends `/start` or `/help` command
     """
-    os.system('cd ESRGAN')
     await message.reply("This bot can style image by pattern of another image. Send 2 pictures: 1) base 2) style")
 
 
-@dp.message_handler(content_types=['photo'])
+
+@dp.message_handler(commands='style', commands_ignore_caption=False, content_types=["document", "photo"])
+async def set_style(message):
+    if message.photo:
+        await message.photo[-1].download(f'style_{message.chat.id}.jpg')
+    else:
+        await bot.send_message(message.chat.id, 'Прикрепите картинку')
+
+
+@dp.message_handler(commands='content', commands_ignore_caption=False, content_types=["document", "photo"])
+async def set_content(message):
+    if message.photo:
+        await message.photo[-1].download(f'content_{message.chat.id}.jpg')
+    else:
+        await bot.send_message(message.chat.id, 'Прикрепите картинку')
+
+
+@dp.message_handler(commands='run')
+async def run(message):
+    stylize(message.chat.id, 100)
+    await bot.send_photo(message.chat.id, types.InputFile(f'res_{message.chat.id}.jpg'))
+
+
+@dp.message_handler(content_types=["document", "photo"])
 async def upscale(message):
-    try:
-        downloadable = message.photo[-1]
-        model = arch.RRDBNet(3, 3, 64, 23, gc=32)
-        model.load_state_dict(torch.load('RRDB_ESRGAN_x4.pth'), strict=True)
-        model.eval()
-        await downloadable.download(destination_file=f'{message.chat.id}.png')
-        img = cv2.imread(f'{message.chat.id}.png', cv2.IMREAD_COLOR)
-        img = img * 1.0 / 255
-        img = torch.from_numpy(np.transpose(img[:, :, [2, 1, 0]], (2, 0, 1))).float()
-        img_LR = img.unsqueeze(0)
-        with torch.no_grad():
-            output = model(img_LR).data.squeeze().float().cpu().clamp_(0, 1).numpy()
-        output = np.transpose(output[[2, 1, 0], :, :], (1, 2, 0))
-        output = (output * 255.0).round()
-        cv2.imwrite(f'{message.chat.id}.png', output)
+    # print(downloadable["file_size"])
+    model = arch.RRDBNet(3, 3, 64, 23, gc=32)
+    model.load_state_dict(torch.load('RRDB_ESRGAN_x4.pth'), strict=True)
+    model.eval()
+    await message.document.download(destination_file=f'{message.chat.id}.png')
+    img = cv2.imread(f'{message.chat.id}.png', cv2.IMREAD_COLOR)
+    img = img * 1.0 / 255
+    img = torch.from_numpy(np.transpose(img[:, :, [2, 1, 0]], (2, 0, 1))).float()
+    img_LR = img.unsqueeze(0)
+    with torch.no_grad():
+        output = model(img_LR).data.squeeze().float().cpu().clamp_(0, 1).numpy()
+    output = np.transpose(output[[2, 1, 0], :, :], (1, 2, 0))
+    output = (output * 255.0).round()
+    cv2.imwrite(f'{message.chat.id}.png', output)
+    await bot.send_photo(message.chat.id, types.InputFile(f'{message.chat.id}.png'))
+    os.remove(f'{message.chat.id}.png')
 
-        # img = Image.open(img_name)
-        # lr_t = tt.ToTensor()(img)[:3]
-        # model = edsr_baseline(2, pretrained=True)
-        # # model = rcan(2, pretrained=True)
-        # # model = edsr(2, pretrained=True)
-        # sr_t = model(lr_t)
-        # sr = tt.ToPILImage()(sr_t.squeeze(0))
-        # sr.save(img_name)
-        await bot.send_photo(message.chat.id, types.InputFile(f'{message.chat.id}.png'))
-        os.remove(f'{message.chat.id}.png')
-        # bot.send_message(message.chat.id, 'Photo does not need rescaling')
-    except Exception as e:
-        print(e)
-
-
-# @dp.message_handler(commands=['set_style'])
-# async def set_style(message):
-#     await message.photo[-1].download('style.jpg')
-#
-#
-# @dp.message_handler(commands=['set_style'])
-# async def set_style(message):
-#     await message.photo[-1].download('style.jpg')
-#
-#
-# @dp.message_handler(commands=['set_content'])
-# async def set_content(message):
-#     await message.photo[-1].download('content.jpg')
-#
-#
-# @dp.message_handler(commands=['run'])
-# async def run(message):
-#     model = Model()
-#     img = model.stylize(100)
-#     await bot.send_photo(message.chat.id, types.InputFile('res.jpg'))
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
---
+++ stylizer.py
@@ -0,0 +1,102 @@
+import torch
+import torch.optim as optim
+from torchvision import transforms, models
+import cv2
+import numpy as np
+from tqdm import tqdm
+import asyncio
+
+
+def load_image(img_path, max_size=400, shape=None):
+    '''
+        Load in and transform an image, making sure the image
+        is <= 400 pixels in the x-y dims.
+    '''
+    image = np.array(cv2.imread(img_path, cv2.IMREAD_COLOR))
+    size = shape if shape else min(max_size, max(image.shape))
+    in_transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Resize(size),
+        transforms.Normalize((0.485, 0.456, 0.406),
+                             (0.229, 0.224, 0.225))])
+    # discard the transparent, alpha channel (that's the :3) and add the batch dimension
+    image = in_transform(image)[:3, :, :].unsqueeze(0)
+    return image
+
+def get_features(image, vgg, layers=None):
+    """
+        Run an image forward through a model and get the features for
+        a set of layers. Default layers are for VGGNet matching Gatys et al (2016)
+    """
+    # if layers is None:
+    layers = {'0': 'conv1_1',
+              '5': 'conv2_1',
+              '10': 'conv3_1',
+              '19': 'conv4_1',
+              '21': 'conv4_2',
+              '28': 'conv5_1'}
+    features = {}
+    x = image
+    for name, layer in vgg._modules.items():
+        x = layer(x)
+        if str(name) in layers:
+            features[layers[name]] = x
+    return features
+
+
+def gram_matrix(m):
+    """
+        Calculate the Gram Matrix of a given tensor
+        Gram Matrix: https://en.wikipedia.org/wiki/Gramian_matrix
+    """
+    b, d, h, w = m.size()
+    m = m.view(d, h * w)
+    gram = torch.mm(m, m.t())
+    return gram
+
+
+def im_convert(m):
+    """
+        Display a tensor as an image.
+    """
+    try:
+        m = m.to("cpu").clone().detach().numpy().squeeze()
+    except Exception as e:
+        pass
+    image = m.transpose(1, 2, 0)
+    image = image * np.array((0.229, 0.224, 0.225)) + np.array((0.485, 0.456, 0.406))
+    return image.clip(0, 1)
+
+
+def stylize(chat_id, steps=100):
+    """
+        Train model to stylize.
+    """
+    print(chat_id)
+    vgg = torch.load('model.h5')
+    style_weights = {'conv1_1': 1., 'conv2_1': 0.8, 'conv3_1': 0.5, 'conv4_1': 0.3, 'conv5_1': 0.1}
+    content_weight = 1
+    style_weight = 1e6
+    content = load_image(f'content_{chat_id}.png')
+    style = load_image(f'style_{chat_id}.png', shape=content.shape[-2:])
+    target = content.clone().requires_grad_(True)
+    optimizer = optim.Adam([target], lr=0.13)
+    content_features = get_features(content, vgg)
+    style_features = get_features(style, vgg)
+    style_grams = {layer: gram_matrix(style_features[layer]) for layer in style_features}
+    for _ in tqdm(range(steps)):
+        target_features = get_features(target, vgg)
+        content_loss = torch.mean((target_features["conv4_2"] - content_features["conv4_2"]) ** 2)
+        style_loss = 0
+        for layer in style_weights:
+            target_feature = target_features[layer]
+            _, d, h, w = target_feature.shape
+            target_gram = gram_matrix(target_feature)
+            style_gram = style_grams[layer]
+            layer_style_loss = style_weights[layer] * torch.mean((target_gram - style_gram) ** 2)
+            style_loss += layer_style_loss / (d * h * w)
+        total_loss = content_weight * content_loss + style_weight * style_loss
+        optimizer.zero_grad()
+        total_loss.backward()
+        optimizer.step()
+    cv2.imwrite(f'res_{chat_id}.png', (im_convert(target) * 255).astype(np.uint8))
